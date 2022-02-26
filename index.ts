c31beee47506ef373b46a2d254bc6135740075a7
kthss01 <kthss01@naver.com>
2022-02-26 17:28:18 +0900

    리엑트 네이티브 개발환경 구축

---
+++ index.ts
@@ -0,0 +1,6 @@
+console.log("Hello, React-Native!")
+console.log("Test Prettier formatting")
+
+// 다음 행 prettier 적용 x
+// prettier-ignore
+console.log("hello");
---
+++ week1/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+export default function App() {
+  console.log('week_1 App call...')
+  const testElement = React.createElement(Text, null, 'Success')
+  // createElement API
+  /*
+    가장 저수준의 가상 DOM 객체를 생성한다.
+  */
+  return testElement
+}
